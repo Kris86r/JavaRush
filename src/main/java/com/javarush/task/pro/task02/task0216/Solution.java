c5f0b453f2706e8ff13cb9e3c66d23a266ced5f9
Kris86r <pechenka869@gmail.com>
2023-01-18 19:12:14 +0500

    Solution

--- src/main/java/com/javarush/task/pro/task02/task0216/Solution.java
+++ src/main/java/com/javarush/task/pro/task02/task0216/Solution.java
@@ -3,7 +3,7 @@ package com.javarush.task.pro.task02.task0216;
 
 public class Solution {
     public static void main(String[] args) {
-        String name = "Taiva", date = "16.01.2023", time = "23:15", adjective = "idk what this", noun = "it is too", verb = "and this";
+        String name = "Taiva", date = "16.01.2023", time = "23:15", adjective = "телефон", noun = "флешку", verb = "считывает";
         System.out.println("Новости:");
         System.out.println(name + " " + date + " " + time + " изобрел " + adjective + " " + noun + " который " + verb + " террабайты данных в минуту.");
     }
