6744fe54ce870844149f24b925aed2e1492f553e
Kris86r <pechenka869@gmail.com>
2023-01-19 22:08:28 +0500

    Solution

--- src/main/java/com/javarush/task/pro/task03/task0308/Solution.java
+++ src/main/java/com/javarush/task/pro/task03/task0308/Solution.java
@@ -7,15 +7,12 @@ public class Solution {
         Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt();
         int y = scanner.nextInt();
-        if (x > 0 && y > 0) {
-            System.out.println("1");
-        } else if (x < 0 && y > 0) {
-            System.out.println("2");
-        } else if (x < 0 && y < 0) {
-            System.out.println("3");
-        } else {
-            System.out.println("4");
-        }
+        if (x > 0 && y > 0) System.out.println("1");
+        if (x < 0 && y > 0) System.out.println("2");
+        if (x < 0 && y < 0) System.out.println("3");
+        if (x > 0 && y < 0) System.out.println("4");
+
+//        System.out.println(y > 0 ? x > 0 ? 1 : 2 : x < 0 ? 3 : 4);
     }
 }
 
