613fce98c50ae00efc0d18f766e4ede236e362c0
Kris86r <pechenka869@gmail.com>
2023-01-23 20:53:17 +0500

    Solution

--- src/main/java/com/javarush/task/pro/task04/task0421/Solution.java
+++ src/main/java/com/javarush/task/pro/task04/task0421/Solution.java
@@ -3,20 +3,15 @@ package com.javarush.task.pro.task04.task0421;
 
 public class Solution {
     public static void main(String[] args) {
-        int i = 100;
+        int i = 0;
         int sum = 0;
-        while (i >= 0) {
-            if (i % 2 == 0) {
-                sum += i;
-            }
-            i--;
+        while (i <= 100) {
+            sum += i;
+            i += 2;
         }
         System.out.println(sum);
     }
 }
-
-
-
 
 
 
