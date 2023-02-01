798d1f3a801bcda9547bbd1e61103c21bd5cc8cd
Kris86r <pechenka869@gmail.com>
2023-02-01 22:43:26 +0500

    Solution

--- src/main/java/com/javarush/task/pro/task05/task0505/Solution.java
+++ src/main/java/com/javarush/task/pro/task05/task0505/Solution.java
@@ -5,24 +5,23 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int num = 0;
-       while (num<scanner.nextInt()){
-           int N = scanner.nextInt();
-           int[] array = new int[N];
-           num++;
+        int N = scanner.nextInt();
+        int[] array = new int[N];
+        if (N > 0) {
             for (int j = 0; j < array.length; j++) {
-                if (array[j] % 2 != 0) {
-                    array[j] = array.length - j;
-                    num = array[j];
-                } else {
-                    num = array[j];
+                array[j] = scanner.nextInt();
+            }
+            if (N % 2 == 0) {
+                for (int i = array.length - 1; i >= 0; i--)
+                    System.out.println(array[i]);
+            } else {
+                for (int j = 0; j < array.length; j++) {
+                    System.out.println(array[j]);
                 }
             }
         }
-        System.out.println(num);
     }
 }
-
 
 
 
