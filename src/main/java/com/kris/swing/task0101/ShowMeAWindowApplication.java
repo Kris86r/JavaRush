90072d5af54293909adaf8f9302411198cff05bb
Kris86r <pechenka869@gmail.com>
2023-02-05 21:57:53 +0500

    Solution

---
+++ src/main/java/com/kris/swing/task0101/ShowMeAWindowApplication.java
@@ -0,0 +1,9 @@
+package com.kris.swing.task0101;
+
+import javax.swing.*;
+
+public class ShowMeAWindowApplication {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Kris loves arrays");
+    }
+}
