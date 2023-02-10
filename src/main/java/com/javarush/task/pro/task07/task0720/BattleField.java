ae4a876633cfc3d9a2179a8afc235cdf1aa55d87
Kris86r <pechenka869@gmail.com>
2023-02-10 17:53:59 +0500

    Solution

--- src/main/java/com/javarush/task/pro/task07/task0720/BattleField.java
+++ src/main/java/com/javarush/task/pro/task07/task0720/BattleField.java
@@ -1,11 +1,12 @@
 package com.javarush.task.pro.task07.task0720;
 
 public class BattleField {
-    public static String galavirAttack = "враг наносит удар";
     public static String nimrodDefence = "вражеская атака отбита";
     public static String nimrodDestroy = "вражеский удар не выдержан";
     public static String galavirDefence = "наша атака успешно отражена врагом";
     public static String galavirDestroy = "вражеский коабль повержен";
+    public static String galavirAttack = "враг наносит удар";
+    public static String nimrodAttack = "Нимрод контратакует";
 
 
     public static void main(String[] args) {
@@ -14,8 +15,13 @@ public class BattleField {
         do {
             System.out.println(galavirAttack);
             nimrod.defend(galavirXIII.attack());
-        } while (isNimrodAlive(nimrod));
-
+            if (isNimrodAlive(nimrod)) {
+                System.out.println(nimrodAttack);
+                galavirXIII.defend(nimrod.attack());
+            } else {
+                break;
+            }
+        } while (isGalavirAlive(galavirXIII));
     }
 
     public static boolean isNimrodAlive(Nimrod nimrod) {
