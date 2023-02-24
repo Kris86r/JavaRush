f04fc5682cacf64c2e11461adfba6361710d9af4
Kris86r <pechenka869@gmail.com>
2023-02-24 20:07:00 +0500

    Solution

---
+++ src/main/java/com/javarush/task/pro/task09/task0902/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.pro.task09.task0902;
+
+public class Solution {
+    public double a = 0.;
+    public double b = .0;
+//    public int c = 100D;
+//    public int d = 100.0;
+//    public int e = 1.11E5;
+//    public int f = 200F;
+//    public int g = 0.F;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
