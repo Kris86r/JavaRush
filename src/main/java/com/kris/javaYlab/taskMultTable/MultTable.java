90d002b606c6f90de414aba58921a289024a9af7
Kris86r <pechenka869@gmail.com>
2023-03-04 18:59:52 +0500

    Solution

--- src/main/java/com/kris/javaYlab/taskMultTable/MultTable.java
+++ src/main/java/com/kris/javaYlab/taskMultTable/MultTable.java
@@ -1,6 +1,6 @@
 package com.kris.javaYlab.taskMultTable;
 
-public class MultiTable {
+public class MultTable {
     public static void main(String[] args) throws Exception {
         for (int i = 1; i <= 9; i++) {
             for (int j = 1; j <= 9; j++) {
