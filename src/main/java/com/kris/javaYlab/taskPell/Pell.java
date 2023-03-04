b3fa2758993e3b62331342fd1eae52e210b6accc
Kris86r <pechenka869@gmail.com>
2023-03-04 18:11:48 +0500

    Solution

---
+++ src/main/java/com/kris/javaYlab/taskPell/Pell.java
@@ -0,0 +1,17 @@
+package com.kris.javaYlab.taskPell;
+
+import java.util.Scanner;
+
+import static java.lang.Math.sqrt;
+
+public class Pell {
+    public static void main(String[] args) throws Exception {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int n = scanner.nextInt();
+            if (n >= 0 && n <= 30) {
+                double pell = ((Math.pow(1 + (sqrt(2)), n)) - (Math.pow(1 - (sqrt(2)), n))) / ((sqrt(2)) + (sqrt(2))) + 1;
+                System.out.println((int) pell);
+            }
+        }
+    }
+}
